69d6357aed27119c38e9202b43c80ce91bb49533
Baranbulduk <baranbulduk99@gmail.com>
2025-07-11 16:37:16 +0200

    Adjustments

--- frontend/src/components/Admin/statistics/Statistics.jsx
+++ frontend/src/components/Admin/statistics/Statistics.jsx
@@ -244,7 +244,7 @@ const Statistics = () => {
   return (
     <div className="max-w-7xl mx-auto px-4 sm:px-6 py-4 sm:py-6 lg:py-8 space-y-6">
       <h1 className="text-3xl font-bold bg-gradient-to-r from-[#FFD08E] via-[#FF6868] to-[#926FF3] bg-clip-text text-transparent dark:bg-gradient-to-r dark:from-[#FFD08E] dark:via-[#FF6868] dark:to-[#926FF3] dark:bg-clip-text dark:text-transparent w-fit mb-6">
-        Statistics Dashboard
+        Statistics
       </h1>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-8">
